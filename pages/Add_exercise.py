83dbb9b3d1fbc8073ca5741a68d58c2d32e278d2
marcgror <marcgrauortiz@gmail.com>
2023-11-18 16:14:31 +0100

    [Add_exercise] Allow muscles to be selected

--- pages/Add_exercise.py
+++ pages/Add_exercise.py
@@ -4,11 +4,18 @@ import pandas as pd
 exercises = pd.read_csv('muscles_targeted.csv', delimiter=',')
 st.markdown('**Current added exercises**')
 st.dataframe(exercises)
+# Get the list of Primary muscles
+primary_muscles = sorted(exercises['Primary'].unique())
+# Get the list of Secondary muscles
+secondary_muscles = exercises['Secondary'].unique()
 st.divider()
 col1, col2, col3 = st.columns(3)
 name = col1.text_input(label='Exercise name:')
-primary = col2.text_input('Primary muscle:')
-secondary = col3.text_input('Secondary muscle (if required):')
+# Display a widget to select the Primary muscle
+primary = col2.selectbox('Primary muscle:', options=primary_muscles)
+# Display a widget to select the Secondary muscle
+secondary = col3.selectbox('Secondary muscle (if required):', options=secondary_muscles)
+# Create a Serie to store data
 temporal_exercise = pd.Series([name, primary, secondary], index=['Exercise', 'Primary', 'Secondary'])
 add_button = st.button('Add exercise')
 st.divider()
